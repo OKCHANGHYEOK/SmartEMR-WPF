2166df2b440c6b2662b2836634ba04431167e664
OK CHANG HYEOK <dkdldy2@naver.com>
2026-05-14 14:07:20 +0900

    feat : 애플리케이션 예외 로그 기능 구현

---
+++ SmartEMR.Application/Core/Logger.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace SmartEMR.Application.Core;
+
+public static class Logger
+{
+    private static readonly string dir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs");
+
+    public static void WriteLog(Exception e)
+    {
+        try
+        {
+            if (!Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+
+            string fileName = $"{DateTime.Now.ToString("yyyy-MM-dd")}.txt";
+            string filePath = Path.Combine(dir, fileName);
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($" {e.Message}");
+            sb.AppendLine($" {e.GetType().FullName}");
+            sb.AppendLine($" {DateTime.Now:yyyy-MM-dd HH:mm:ss}");
+            sb.AppendLine("===============================================================");
+            sb.AppendLine(e.StackTrace);
+            sb.AppendLine(); 
+            sb.AppendLine();
+
+            File.AppendAllText(filePath, sb.ToString());
+
+        } catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+        }
+    }
+}
