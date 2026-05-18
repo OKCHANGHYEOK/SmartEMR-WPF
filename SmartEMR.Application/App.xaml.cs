ad706fc441c784d6e6d66241414cd25c0ae6bf8b
OK CHANG HYEOK <dkdldy2@naver.com>
2026-05-18 15:38:37 +0900

    feat : 애플리케이션 중복 실행 방지 기능 구현

--- SmartEMR.Application/App.xaml.cs
+++ SmartEMR.Application/App.xaml.cs
@@ -14,10 +14,24 @@ namespace SmartEMR.Application
     /// </summary>
     public partial class App : System.Windows.Application
     {
+        private static readonly string AppName = "Global\\SmartEMR_Application_Unique_Mutex_Key_2026";
+        private static Mutex? _mutex;
+
         private int? MUR_Idx { get; set; } = 100000;
 
         protected override void OnStartup(StartupEventArgs e)
         {
+            _mutex = new Mutex(true, AppName, out bool isNewInstance);
+
+            if (!isNewInstance)
+            {
+                MessageBox.Show("SmartEMR이 이미 실행중입니다.", "알림", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                _mutex.Dispose();
+                Shutdown();
+                return;
+            }
+
             base.OnStartup(e);
         
             LoginWindow loginWindow = new LoginWindow();
@@ -56,6 +70,17 @@ namespace SmartEMR.Application
 
             this.MainWindow = new vLayout(typeof(vSmartEMRDeskTab));
             this.MainWindow.Show();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            if (_mutex != null)
+            {
+                _mutex.ReleaseMutex();
+                _mutex.Dispose();   
+            }
+
+            base.OnExit(e);
         }
     }
 
