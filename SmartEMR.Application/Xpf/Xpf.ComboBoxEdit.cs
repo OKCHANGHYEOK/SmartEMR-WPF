7f1433f692c64198d1249a027b5126dcd037a8f2
OK CHANG HYEOK <dkdldy2@naver.com>
2026-06-23 09:26:51 +0900

    fix : 콤보박스 팝업 표시 동작 수

--- SmartEMR.Application/Xpf/Xpf.ComboBoxEdit.cs
+++ SmartEMR.Application/Xpf/Xpf.ComboBoxEdit.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Windows.Input;
+using DevExpress.Xpf.Editors;
 
 namespace SmartEMR.Application.Xpf;
 
@@ -8,22 +9,25 @@ public class ComboBoxEdit : DevExpress.Xpf.Editors.ComboBoxEdit
     {
         this.MinHeight = 20;
         this.MinWidth = 40;
-        this.VerticalAlignment = VerticalAlignment.Stretch;
-        this.HorizontalAlignment = HorizontalAlignment.Center;
+        this.VerticalAlignment = System.Windows.VerticalAlignment.Stretch;
+        this.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
         this.ShowNullTextForEmptyValue = false;
         this.IsTextEditable = false;
-        this.PreviewMouseLeftButtonDown += ComboBoxEdit_PreviewMouseLeftButtonDown;
+
+        // ⭐️ [해결 코드] 마우스로 클릭할 때 레이아웃 충돌로 팝업이 씹히는 현상을 방지합니다.
+        this.PreviewMouseDown += ComboBoxEdit_PreviewMouseDown;
     }
 
-    private void ComboBoxEdit_PreviewMouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+    private void ComboBoxEdit_PreviewMouseDown(object sender, MouseButtonEventArgs e)
     {
-        var cmb = sender as ComboBoxEdit;
-        if (cmb == null) return;
+        if (sender is not ComboBoxEdit cmb) return;
 
-        if (cmb.ItemsSource != null) 
+        // 이미 팝업이 열려있다면 닫아주고, 닫혀있다면 강제로 이벤트를 선점하여 확실하게 열어줍니다.
+        if (!cmb.IsPopupOpen)
         {
+            cmb.Focus();
             cmb.ShowPopup();
-            e.Handled = true;
+            e.Handled = true; // 💡 이벤트가 위로 올라가서 다른 갱신 로직과 타이밍 꼬이는 것을 방지
         }
     }
 }
