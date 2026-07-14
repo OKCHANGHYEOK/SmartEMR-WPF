0f0e17e12b473e71b28f260adab3d45a5c525e2e
OK CHANG HYEOK <dkdldy2@naver.com>
2026-07-14 11:07:08 +0900

    - 누락 변경사항 커밋

--- SmartEMR.Application/ViewModels/SmartEMRDeskRCPViewModel.cs
+++ SmartEMR.Application/ViewModels/SmartEMRDeskRCPViewModel.cs
@@ -83,27 +83,7 @@ public class SmartEMRDeskRCBViewModel : BaseViewModel<ReceptionBoard>
 
         if (retRCB != null && retRCB.Any())
         {
-            foreach (var mitem in retRCB)
-            {
-                mitem.MUR_Name_DOC = mitem.MUR_Name_DOC ?? "-";
-                mitem.vPAT_Sex = mitem.PAT_Sex == "M" ? "남" : "여";
-                mitem.vPAT_Info = $"{mitem.vPAT_Sex}/{mitem.PAT_Age.GetValueOrDefault(0)}";
-                mitem.vRCB_Type = mitem.RCB_Type == "RES" ? "예약" : mitem.RCB_Type == "RCP" ? "접수" : "";
-                mitem.vRCB_Subject = SmartMVVM.Common.GetCommonCodeName("RCB", "Subject", mitem.RCB_Subject ?? "");
-
-                if (mitem.RCB_Type == "RES")
-                {
-                    mitem.vRES_Status = SmartMVVM.Common.GetCommonCodeName("RES", "Status", mitem.RES_Status ?? "")?.Substring(2);
-                    mitem.vRCP_Status = "-";
-                }
-                else if (mitem.RCB_Type == "RCP")
-                {
-                    mitem.vRES_Status = "-";
-                    mitem.vRCP_Status = SmartMVVM.Common.GetCommonCodeName("RCP", "Status", mitem.RCP_Status ?? "")?.Substring(2);
-                    mitem.vRCP_InsuranceType = SmartMVVM.Common.GetCommonCodeName("RCP", "InsuranceType", mitem.RCP_InsuranceType ?? "")?.Substring(0,1);
-                    mitem.vRCB_VisitType = SmartMVVM.Common.GetCommonCodeName("RCP", "VisitType", mitem.RCB_VisitType ?? "");
-                }
-            }
+            SmartMVVM.ProcessorProvider.ReceptionBoardProcessor.Process(retRCB);
 
             arrRCB = retRCB.ToList();
         }
