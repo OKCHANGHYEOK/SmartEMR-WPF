090aa13457a31bdd6051bff99bba6b4222e6adfa
OK CHANG HYEOK <dkdldy2@naver.com>
2026-08-15 16:12:59 +0900

    feat : RichTextEdit 관련 기능 구현
     - RichTextEdit 툴바에서 볼드, 이텔릭체, 언더라인 및 글꼴변경 기능 구현

--- SmartEMR.Application/Xpf/Xpf.RichTextEdit.cs
+++ SmartEMR.Application/Xpf/Xpf.RichTextEdit.cs
@@ -1,30 +1,65 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using DevExpress.Xpf.Editors;
+using DevExpress.Xpf.RichEdit;
+using DevExpress.XtraRichEdit.API.Native;
 
 namespace SmartEMR.Application.Xpf;
 
 public class RichTextEdit : UserControl
 {
-    //public static readonly DependencyProperty TextProperty =
-    //    DependencyProperty.Register(nameof(Text), typeof(string), typeof(RichTextEdit), new PropertyMetadata(string.Empty));
-
-    //public string Text
-    //{
-    //    get => (string)GetValue(TextProperty);
-    //    set => SetValue(TextProperty, value);
-    //}
-
-    //public static readonly DependencyProperty RtfTextProperty =
-    //    DependencyProperty.Register(nameof(RtfText), typeof(string), typeof(RichTextEdit), new PropertyMetadata(string.Empty));
-
-    //public string RtfText
-    //{
-    //    get => (string)GetValue(RtfTextProperty);
-    //    set => SetValue(RtfTextProperty, value);
-    //}
-
     static RichTextEdit()
     {
         DefaultStyleKeyProperty.OverrideMetadata(typeof(RichTextEdit), new FrameworkPropertyMetadata(typeof(RichTextEdit)));
+    }
+
+    private ComboBoxEdit? _fontFamilyComboBox;
+    private RichEditControl? _richEdit;
+
+    public override void OnApplyTemplate()
+    {
+        // 기존 이벤트 연결 제거
+        if (_fontFamilyComboBox is not null)
+        {
+            _fontFamilyComboBox.EditValueChanged -= OnEditValueChanged_FontFamily;
+        }
+
+        base.OnApplyTemplate();
+
+        // Template 내부 컨트롤 가져오기
+        _fontFamilyComboBox = GetTemplateChild("cmbFontFamiliy") as ComboBoxEdit;
+        _richEdit = GetTemplateChild("RichEdit") as RichEditControl;
+
+        // 이벤트 연결
+        if (_fontFamilyComboBox is not null)
+        {
+            _fontFamilyComboBox.EditValueChanged += OnEditValueChanged_FontFamily;
+        }
+    }
+
+    private void OnEditValueChanged_FontFamily(object sender, EditValueChangedEventArgs e)
+    {
+        if (_richEdit is null) return;
+
+        // 현재 선택 영역
+        DocumentRange range = _richEdit.Document.Selection;
+
+        // 선택 영역이 없으면 일단 무시 -> 선택 영역 없을 때 폰트 바꿔놓고 입력하는 경우 처리해야함
+        if (range.Length == 0) return;
+
+        string? fontName = e.NewValue?.ToString();
+
+        if (string.IsNullOrWhiteSpace(fontName)) return;
+
+        CharacterProperties properties = _richEdit.Document.BeginUpdateCharacters(range);
+
+        try
+        {
+            properties.FontName = fontName;
+        }
+        finally
+        {
+            _richEdit.Document.EndUpdateCharacters(properties);
+        }
     }
 }
