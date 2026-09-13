a4f8a431b1baee5103357f277aee162582f86cf3
OK CHANG HYEOK <dkdldy2@naver.com>
2026-09-13 17:48:44 +0900

    feat : 수납 페이지 추가
     - 수납탭 페이지 추가

---
+++ SmartEMR.Application/Views/SmartEMRPay/vSmartEMRPayTab.xaml.cs
@@ -0,0 +1,26 @@
+﻿using SmartEMR.Application.ViewBase;
+using SmartEMR.Application.ViewModels;
+using SmartEMR.Application.Xpf;
+
+namespace SmartEMR.Application.Views.SmartEMRPay.Views
+{
+    /// <summary>
+    /// vSmartEMRPayTab.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class vSmartEMRPayTab : ModelViewLayout<PayViewModel>
+    {
+        public vSmartEMRPayTab() { }
+
+        protected override void Initialize()
+        {
+        }
+
+        public override void OnBindGrid_BindClick(object? sender, BindClickEventArgs e)
+        {
+        }
+
+        public override void OnBindGrid_BindItemChanged(object? sender, BindItemChangedEventArgs e)
+        {
+        }
+    }
+}
